30fe2405ac86ace8c32eb1424844d02fb88da412
Nikita Koruts <nikitakoruts@gmail.com>
2024-01-29 10:08:29 +0200

    Add CategoryRoundView

---
+++ Repeatly/Modules/Home/View/CategoryRoundView.swift
@@ -0,0 +1,27 @@
+//
+//  CategoryRoundView.swift
+//  Repeatly
+//
+//  Created by Nikita Koruts on 29.01.2024.
+//
+
+import SwiftUI
+
+struct CategoryRoundView: View {
+    let title: String
+    let color: Color
+    
+    var body: some View {
+        Text(title)
+            .font(.gilroyRegular(size: 14))
+            .foregroundColor(.white)
+            .lineLimit(1)
+            .padding(.horizontal, 12)
+            .padding(.vertical, 4)
+            .background(
+                //                RoundedRectangle(cornerRadius: 8)
+                Capsule()
+                    .fill(color)
+            )
+    }
+}
