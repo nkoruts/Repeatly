e90fa5d9d8dd242a0363721c763ffe7743f2a442
Nikita Koruts <nikitakoruts@gmail.com>
2024-01-29 21:13:54 +0200

    Init SettingsView

---
+++ Repeatly/Modules/SettingsView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsView.swift
+//  Repeatly
+//
+//  Created by Nikita Koruts on 29.01.2024.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
