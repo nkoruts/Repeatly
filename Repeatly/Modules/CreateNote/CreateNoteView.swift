50c3aa7c0e7a229b8d67196b02b2e151b975f52a
Nikita Koruts <nikitakoruts@gmail.com>
2024-01-29 21:13:02 +0200

    Init CreateNoteView

---
+++ Repeatly/Modules/CreateNote/CreateNoteView.swift
@@ -0,0 +1,20 @@
+//
+//  CreateNoteView.swift
+//  Repeatly
+//
+//  Created by Nikita Koruts on 29.01.2024.
+//
+
+import SwiftUI
+
+struct CreateNoteView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CreateNoteView_Previews: PreviewProvider {
+    static var previews: some View {
+        CreateNoteView()
+    }
+}
