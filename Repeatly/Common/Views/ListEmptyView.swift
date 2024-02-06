e339c5698ba4eccf31c3084882f94305552d14a1
Nikita Koruts <nikitakoruts@gmail.com>
2024-02-06 08:37:53 +0200

    Add list empty view

---
+++ Repeatly/Common/Views/ListEmptyView.swift
@@ -0,0 +1,44 @@
+//
+//  ListEmptyView.swift
+//  Repeatly
+//
+//  Created by Nikita Koruts on 05.02.2024.
+//
+
+import SwiftUI
+
+struct ListEmptyView: View {
+    var title: String
+    var description: String
+    
+    var body: some View {
+        VStack(spacing: 16) {
+            Spacer()
+            
+            Image(systemName: "tray")
+                .font(.system(size: 64))
+                .foregroundColor(ColorSystem.icon)
+            VStack(spacing: 4) {
+                Text(title)
+                    .font(.gilroySemiBold(size: 24))
+                    .foregroundColor(ColorSystem.mainText)
+                .lineLimit(1)
+                Text(description)
+                    .font(.gilroyRegular(size: 14))
+                    .foregroundColor(ColorSystem.mainText)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 32)
+            }
+            
+            Spacer()
+        }
+    }
+}
+
+struct ListEmptyView_Previews: PreviewProvider {
+    static var previews: some View {
+        ListEmptyView(
+            title: "Your notes is empty",
+            description: "Create a new Note to get started on spaced repetition")
+    }
+}
