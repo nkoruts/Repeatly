7281454f7d87361a210521ee44a80f2221b38187
Nikita Koruts <nikitakoruts@gmail.com>
2024-02-13 15:22:09 +0200

    Add error logher

---
+++ Repeatly/Common/Utility/Logger.swift
@@ -0,0 +1,12 @@
+//
+//  Logger.swift
+//  Repeatly
+//
+//  Created by Nikita Koruts on 13.02.2024.
+//
+
+import Foundation
+
+func logError(_ error: Error) {
+    print("=== Debug Log: \(error.localizedDescription)")
+}
