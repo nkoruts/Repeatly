14e09f622b51a7b63b951f00ee4b3d38a261f305
Nikita Koruts <nikitakoruts@gmail.com>
2024-02-18 18:34:43 +0200

    Add swipe action

--- Repeatly/Modules/Home/Views/CardView.swift
+++ Repeatly/Modules/Home/Views/CardView.swift
@@ -15,49 +15,98 @@ struct CardViewModel {
 struct CardView: View {
     var viewModel: CardViewModel
     
+    @State var cardOffset: CGFloat = 0
+    @State var isSwipped: Bool = false
+    
     var body: some View {
-        HStack(spacing: Constants.mainStackSpacing) {
-            Capsule()
-                .fill(Color(viewModel.note.color))
-                .frame(width: Constants.dividerWidth)
-                .padding(.leading, Constants.dividerLeadingPadding)
-                .padding(.vertical, Constants.dividerVerticalPadding)
+        ZStack {
+            Color.red
             
-            VStack(alignment: .leading, spacing: Constants.infoSpacing) {
-//                if let category = viewModel.note.category {
-//                    NoteCategoryView(
-//                        title: category.name,
-//                        color: Color(category.color))
-//                    .padding(.bottom, Constants.infoSpacing)
-//                }
+            HStack {
+                Spacer()
                 
-                Text(viewModel.note.title)
-                    .font(FontBook.medium3)
-                    .foregroundColor(ColorSystem.mainText.color)
-                
-                if let details = viewModel.note.details {
-                    Text(details)
-                        .font(FontBook.regular4)
-                        .foregroundColor(ColorSystem.grayText.color)
-                        .lineLimit(Constants.detailsLineLimit)
+                Button(action: {}) {
+                    Image(systemName: "trash")
+                        .font(.title)
+                        .foregroundColor(.white)
+                        .frame(width: 90, height: 50)
                 }
             }
-            .padding([.vertical, .trailing], Constants.defaultPadding)
             
-            Spacer()
-            
-            Image(systemName: Constants.arrowIconName)
-                .foregroundColor(ColorSystem.icon.color)
-                .padding(.trailing, Constants.defaultPadding)
+            HStack(spacing: Constants.mainStackSpacing) {
+                Capsule()
+                    .fill(Color(viewModel.note.color))
+                    .frame(width: Constants.dividerWidth)
+                    .padding(.leading, Constants.dividerLeadingPadding)
+                    .padding(.vertical, Constants.dividerVerticalPadding)
+                
+                VStack(alignment: .leading, spacing: Constants.infoSpacing) {
+                    if let category = viewModel.category {
+                        NoteCategoryView(
+                            title: category.name,
+                            color: Color(category.color))
+                        .padding(.bottom, Constants.infoSpacing)
+                    }
+                    
+                    Text(viewModel.note.title)
+                        .font(FontBook.medium2)
+                        .foregroundColor(ColorSystem.mainText.color)
+                    
+                    if let details = viewModel.note.details {
+                        Text(details)
+                            .font(FontBook.regular3)
+                            .foregroundColor(ColorSystem.grayText.color)
+                            .lineLimit(Constants.detailsLineLimit)
+                    }
+                }
+                .padding([.vertical, .trailing], Constants.defaultPadding)
+                
+                Spacer()
+                
+                Image(systemName: Constants.arrowIconName)
+                    .foregroundColor(ColorSystem.icon.color)
+                    .padding(.trailing, Constants.defaultPadding)
+            }
+            .background(
+                RoundedRectangle(
+                    cornerRadius: DesignSystem.cornerRadius,
+                    style: .continuous)
+                .fill(ColorSystem.cardBackground.color))
+            .shadow(
+                color: ColorSystem.shadow.color,
+                radius: Constants.shadowRadius)
+            .contentShape(Rectangle())
+            .offset(x: cardOffset)
+            .gesture(
+                DragGesture()
+                    .onChanged(dragOnChange(_:))
+                    .onEnded(dragOnEnd(_:)))
         }
-        .background(ColorSystem.cardBackground.color)
-        .clipShape(
-            RoundedRectangle(
-                cornerRadius: Constants.cornerRadius,
-                style: .continuous)
-        )
-        .shadow(color: ColorSystem.shadow.color, radius: Constants.shadowRadius)
-        
+    }
+    
+    func dragOnChange(_ value: DragGesture.Value) {
+        let offset = value.translation.width
+        guard offset < 0 else { return }
+        cardOffset = isSwipped ? (offset - 90) : offset
+    }
+    
+    func dragOnEnd(_ value: DragGesture.Value) {
+        let offset = value.translation.width
+        withAnimation(.easeInOut) {
+            guard offset < 0 else {
+                cardOffset = .zero
+                return
+            }
+            if -offset > UIScreen.main.bounds.width / 2 {
+                cardOffset = -1000
+            } else if -cardOffset > 50 {
+                isSwipped = true
+                cardOffset = -90
+            } else {
+                isSwipped = false
+                cardOffset = .zero
+            }
+        }
     }
 }
 
@@ -65,7 +114,6 @@ extension CardView {
     private enum Constants {
         static let mainStackSpacing: CGFloat = 12
         static let defaultPadding: CGFloat = 16
-        static let cornerRadius: CGFloat = 18
         static let shadowRadius: CGFloat = 8
         
         static let dividerWidth: CGFloat = 4
