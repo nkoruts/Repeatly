17b0fe45dad90d9c57788f636b8fb5e5f7717b18
Nikita Koruts <nikitakoruts@gmail.com>
2024-05-21 16:45:39 +0300

    Add ColorPickerView

--- Repeatly/Modules/CreateCategory/CreateCategoryView.swift
+++ Repeatly/Modules/CreateCategory/CreateCategoryView.swift
@@ -8,33 +8,36 @@
 import SwiftUI
 
 struct CreateCategoryView: View {
-    var body: some View {
-        NavigationStack {
-            Text("New category")
-                .foregroundColor(.mainText)
-                .font(FontBook.medium)
-                .multilineTextAlignment(.center)
-                .padding(.top, 12)
-            
-            Section(content: {
-                colorsGrid
-            }, header: {
-                SectionHeaderView(title: Constants.colorsTitle)
-                    .padding(.horizontal)
-            })
-            
-            Spacer()
-        }
-    }
     
-    private var colorsGrid: some View {
-        HStack() {
-            ForEach(Constants.colors, id: \.self) { color in
-                color
-                    .frame(width: 60, height: 60)
-                    .cornerRadius(DesignSystem.cornerRadius)
+    @State private var name: String = ""
+    
+    var body: some View {
+        NavigationView {
+            VStack(spacing: Constants.contentSpacing) {
+                Text("New category")
+                    .foregroundColor(.mainText)
+                    .font(FontBook.medium)
+                    .multilineTextAlignment(.center)
+                    .padding(.top, 12)
+                
+                Section(content: {
+                    TextField(Constants.namePlaceholder, text: $name)
+                        .textLimit(Constants.nameTextLength, $name)
+                        .textFieldStyle(BorderedTextFieldStyle())
+                        .submitLabel(.next)
+                }, header: {
+                    SectionHeaderView(title: Constants.categoryNameTitle)
+                })
+                
+                Section(content: {
+                    ColorPickerView(colors: Constants.colors)
+                }, header: {
+                    SectionHeaderView(title: Constants.colorsTitle)
+                })
+                
+                Spacer()
             }
-//            Spacer()
+            .padding(.horizontal)
         }
     }
 }
@@ -42,12 +45,23 @@ struct CreateCategoryView: View {
 // MARK: - Constants
 extension CreateCategoryView {
     private enum Constants {
+        static let contentSpacing: CGFloat = 12
         static let colorsTitle = "Choose color"
+        static let categoryNameTitle = "Category name"
+        static let namePlaceholder = "Example: Study"
+        static let nameTextLength = 6
+        
         static let colors: [Color] = [
             .red,
             .blue,
             .green,
-            .orange
+            .orange,
+            .pink,
+            .purple,
+            .brown,
+            .cyan,
+            .indigo,
+            .mint
         ]
     }
 }
