e3fcb5402a8cb811ba216d36a13d68e649fd44e6
Nikita Koruts <nikitakoruts@gmail.com>
2024-05-28 19:59:05 +0300

    Update navigation

--- Repeatly/Common/Extensions/View+Ext.swift
+++ Repeatly/Common/Extensions/View+Ext.swift
@@ -8,24 +8,6 @@
 import SwiftUI
 
 extension View {
-    // TODO: - Update navigation
-    func navigate<AnyView: View>(to view: AnyView, when binding: Binding<Bool>) -> some View {
-        NavigationView {
-            ZStack {
-                self
-                NavigationLink(
-                    destination: view.navigationBarHidden(true),
-                    isActive: binding) {
-                    EmptyView()
-                }
-            }
-            
-            .navigationBarBackButtonHidden(true)
-        }
-    }
-}
-
-extension View {
     func textLimit(_ limit: Int, _ text: Binding<String>) -> some View {
         self.onChange(of: text.wrappedValue) { _ in
             text.wrappedValue = String(text.wrappedValue.prefix(limit))
