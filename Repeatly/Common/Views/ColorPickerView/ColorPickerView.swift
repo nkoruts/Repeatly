ab50b50a3c7571333c01520a544b63be09943a8f
Nikita Koruts <nikitakoruts@gmail.com>
2024-05-28 19:59:47 +0300

    Update ColorPicker

--- Repeatly/Common/Views/ColorPickerView/ColorPickerView.swift
+++ Repeatly/Common/Views/ColorPickerView/ColorPickerView.swift
@@ -9,11 +9,30 @@ import SwiftUI
 
 struct ColorPickerView: View {
     
-    let colorsHex: [Int32]
+//    let colorsHex: [Int32]
     @Binding var selectedColorHex: Int32?
     
     private let columns: [GridItem] = [.init(.adaptive(minimum: 40))]
+    private let colorsHex: [Int32] = [
+        0xC62828,
+        0xF79F36,
+        0x3D51B2,
+        0x673AB7,
+        0x2196F3
+        
+        //        0x00BCD4,
+        //        0x4CAF50,
+        //        0xF44336,
+        //        0xE91E63
+                
+        //        0x7C4DFF,
+        //        0x03DAC5,
+        //        0xFFA726,
+        //        0x9C27B0,
+        //        0x673AB7
+    ]
     
+    // MARK: - UI
     var body: some View {
         LazyVGrid(columns: columns, spacing: 12) {
             ForEach(colorsHex, id: \.self) { hex in
