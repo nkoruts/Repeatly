aa1b2c75779ed418b653f5f6a2f1b1aea91298ff
Nikita Koruts <nikitakoruts@gmail.com>
2024-10-09 08:20:10 +0300

    Create SearchTextView

---
+++ Repeatly/Modules/Home/Views/SearchTextView.swift
@@ -0,0 +1,43 @@
+//
+//  SearchView.swift
+//  Repeatly
+//
+//  Created by Nikita Koruts on 07.10.2024.
+//
+
+import SwiftUI
+
+struct SearchTextView: View {
+    @Binding var searchText: String
+    let placeholder: String
+    let textLength = 10
+    let cancelAction: Action
+    
+    var body: some View {
+        HStack {
+            TextField(placeholder, text: $searchText)
+                .textLimit(textLength, $searchText)
+                .textFieldStyle(BorderedTextFieldStyle())
+                .submitLabel(.search)
+            Button(action: {
+                cancelAction()
+            }, label: {
+                Image(systemName: "xmark.circle.fill")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 16, height: 16)
+                    .foregroundColor(.icon)
+            })
+            .buttonStyle(.plain)
+        }
+    }
+}
+
+#Preview {
+    @State var searchText = ""
+    return SearchTextView(
+        searchText: $searchText,
+        placeholder: "Search text") {
+            
+        }
+}
