b674e7043bce0ec7dbfa42c08f0bf30376979568
Nikita Koruts <nikitakoruts@gmail.com>
2024-10-11 09:36:35 +0300

    Update RepetitionManager

--- Repeatly/CoreLayer/RepetitionManager.swift
+++ Repeatly/CoreLayer/RepetitionManager.swift
@@ -9,13 +9,13 @@ import Foundation
 
 struct RepetitionModel {
     var nextDate: Date
-    var dayIntervals: [UInt16]
+    var dayIntervals: [Int16]
 }
 
 struct RepetitionManager {
     static var defaultRepetition: RepetitionModel {
         let nextDate = Date()
-        let repetitionIntervals: [UInt16] = [1, 3, 7, 14, 28, 56]
+        let repetitionIntervals: [Int16] = [1, 3, 7, 14, 28, 56]
         return RepetitionModel(nextDate: nextDate, dayIntervals: repetitionIntervals)
     }
 }
